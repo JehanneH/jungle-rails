c1b8e413f4da685d0ceb75f32d2581c14cb16e5c
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-27 17:16:09 -0400

    check if error messages are correct

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,24 +15,28 @@ RSpec.describe Product, type: :model do
     it "product is not valid without name" do
       subject.name = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Name can't be blank"
     end
 
     # validates :price, presence: true
     it "product is not valid without price" do
       subject.price_cents = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Price can't be blank"
     end
 
     # validates :quantity, presence: true
     it "product is not valid without quantity" do
       subject.quantity = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Quantity can't be blank"
     end
 
     # validates :category, presence: true
     it "product is not valid without category" do
       subject.category = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Category can't be blank"
     end
     
     it "is valid with valid attributes" do
