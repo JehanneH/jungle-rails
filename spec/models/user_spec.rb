cc8262d23d3e9e317ffa9f2e11b0a485974dc453
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-27 18:34:04 -0400

    check that all fields will be invalid if left blank

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
       expect(subject).to be_valid
     end
 
-    it "it validates that email, first name, last name, password, and password confirmation are present" do
+    it "validates that email, first name, last name, password, and password confirmation are present" do
       expect(subject.first_name).to be_present
       expect(subject.last_name).to be_present
       expect(subject.email).to be_present
@@ -22,31 +22,37 @@ RSpec.describe User, type: :model do
       expect(subject.password_confirmation).to be_present
     end
 
-    it "user is not valid without first name" do
+    it "is not valid without first name and error message is correct" do
       subject.first_name = nil
       expect(subject).to_not be_valid
-      # puts subject.errors.full_messages
+      expect(subject.errors.full_messages).to include "First name can't be blank"
     end
    
-    it "user is not valid without last name" do
+    it "is not valid without last name and error message is correct" do
       subject.last_name = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Last name can't be blank"
     end
 
-    it "user is not valid without email" do
+    it "is not valid without email and error message is correct" do
       subject.email = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Email can't be blank"
     end
 
-    it "user is not valid without password" do
+    it "is not valid without password and error message is correct" do
       subject.password = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Password can't be blank"
     end
-    
-    it "user is not valid without password" do
-      subject.password = nil
+
+    it "is not valid without password confirmation" do
+      subject.password_confirmation = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Password confirmation can't be blank"
     end
+
+    it ""
 
   end
 end
