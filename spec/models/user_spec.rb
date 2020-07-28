9e4c7639481b773cb65d3359c1d450322e3b4ad1
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-27 21:07:11 -0400

    refactor test to use create instead of new to avoid writing subject.save

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject {
-    described_class.new(first_name: "Jehanne",
-                        last_name: "Hodge",
-                        email: "jh@email.com",
-                        password: "password",
-                        password_confirmation: "password")
+    described_class.create(first_name: "Jehanne",
+                          last_name: "Hodge",
+                          email: "jh@email.com",
+                          password: "password",
+                          password_confirmation: "password")
   }
 
   describe 'Validations' do
@@ -53,49 +53,44 @@ RSpec.describe User, type: :model do
     end
 
     it "is not valid when the email is not unique (not case senstive)" do
-      first_subject = User.new(
+      first_subject = User.create(
         first_name: "New", 
         last_name: "User", 
         email: "test@test.com", 
         password: "password", 
         password_confirmation: "password"
       )
-      first_subject.save
 
-      second_subject = User.new(
+      second_subject = User.create(
         first_name: "Another", 
         last_name: "Person", 
         email: "TEST@TEST.com", 
         password: "password", 
         password_confirmation: "password"
       )
-      second_subject.save
       expect(second_subject).to_not be_valid
       expect(second_subject.errors.full_messages).to include "Email has already been taken"
     end
 
     it "is not valid if password length is too short (less than 3 characters)" do
-      new_subject = User.new(
+      new_subject = User.create(
         first_name: "Another", 
         last_name: "Person", 
         email: "tes@test.com", 
         password: "no", 
         password_confirmation: "no"
       )
-      new_subject.save
       expect(new_subject).to_not be_valid
       expect(new_subject.errors.full_messages).to include "Password is too short (minimum is 3 characters)"
     end
     
     describe '.authenticate_with_credentials' do
       it "authenticates when email is correct but contains empty spaces on either side" do
-       subject.save
        authenticate = User.authenticate_with_credentials(" jh@email.com ", subject.password)
        expect(authenticate).to eq subject
       end
 
       it "authenticates when email is correct but user has used wrong case" do
-        subject.save
         authenticate = User.authenticate_with_credentials("JH@EMAIL.com", subject.password)
         expect(authenticate).to eq subject
        end
